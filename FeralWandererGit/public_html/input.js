5c7bc6faee7e61ae19c4fe9a1f6e1daa7a468e43
Bird <awol4444@gmail.com>
2017-07-25 11:21:35 -0400

    0.0.0.2 merge

---
+++ FeralWandererGit/public_html/input.js
@@ -0,0 +1,12 @@
+/* 
+ * Class that handles all player input.
+ */
+
+// Receive clicks from all the main buttons
+// What about custom dropdowns? Actually, I should create another 'screen' (div) that pops up for lists... Since that's static, it's fine.
+
+//TODO: List every input button. Then create the new div.
+
+// top1 top2 top3 top4 top5 bot1 bot2 bot3 bot4 bot5 quickItem1B quickItem2B next bag
+// Afterwards, some sort of flexible input for the 1-255 or whatever of a dropdown/list.
+// Actually, check how events work in Javascript.
